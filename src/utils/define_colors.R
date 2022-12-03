c2aa1fda29ed9101fe5623d89a079123d8fd7ac3
danielseussler <daniel.seussler@gmail.com>
2022-12-03 12:17:52 +0100

    Create define_colors.R

---
+++ src/utils/define_colors.R
@@ -0,0 +1,15 @@
+# define colors
+c_light_teal = c("#6B8E8E")
+c_mid_teal = c("#487575")
+c_dark_teal = c("#1D4F4F")
+
+c_light = c("#DCBCBC")
+c_light_highlight = c("#C79999")
+c_mid = c("#B97C7C")
+c_mid_highlight = c("#A25050")
+c_dark = c("#8F2727")
+c_dark_highlight = c("#7C0000")
+
+c_light_trans = c("#DCBCBC80")
+c_dark_trans = c("#8F272780")
+c_green_trans = c("#00FF0080")
